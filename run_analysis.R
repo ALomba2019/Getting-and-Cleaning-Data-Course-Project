2214297fcb28f4d207bd89a12e576bf35d7c8f19
BadFrameRate <rohatgi.aseem03@gmail.com>
2020-09-19 17:49:50 +0530

    merged train and test data into one data frame, test and train labels into another; then merged the two data frames

--- run_analysis.R
+++ run_analysis.R
@@ -23,3 +23,9 @@ train_label<-read.table("./UCI HAR Dataset/train/y_train.txt")
 #Read test and train and test data sets
 test_set<-read.table("./UCI HAR Dataset/test/X_test.txt", col.names=ft_names[,2])
 train_set<-read.table("./UCI HAR Dataset/train/X_train.txt", col.names=ft_names[,2])
+
+#Merge train and test sets and labels into one data frame
+merged_set<-rbind(train_set,test_set)
+merged_label<-rbind(train_label,test_label)
+merged_data<-cbind(merged_set,"label"=merged_label)
+
