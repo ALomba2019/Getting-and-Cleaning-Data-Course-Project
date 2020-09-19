9d7c4b9e2e6ff0503e3fc0b56de3df95c84cadff
BadFrameRate <rohatgi.aseem03@gmail.com>
2020-09-19 18:19:58 +0530

    reread data, specying column names at outset

--- run_analysis.R
+++ run_analysis.R
@@ -13,25 +13,24 @@ ft_names<-read.table("./UCI HAR Dataset/features.txt")
 activity_labels<-read.table("./UCI HAR Dataset/activity_labels.txt", col.names=c("n","label"))
 
 #Read subject numbers for the data sets
-subjects_test<-read.table("./UCI HAR Dataset/test/subject_test.txt")
-subjects_train<-read.table("./UCI HAR Dataset/train/subject_train.txt")
+subjects_test<-read.table("./UCI HAR Dataset/test/subject_test.txt", col.names="subjects")
+subjects_train<-read.table("./UCI HAR Dataset/train/subject_train.txt", col.names="subjects")
 
 #Read test and train activity labels
-test_label<-read.table("./UCI HAR Dataset/test/y_test.txt")
-train_label<-read.table("./UCI HAR Dataset/train/y_train.txt")
+test_label<-read.table("./UCI HAR Dataset/test/y_test.txt", col.names= "activity")
+train_label<-read.table("./UCI HAR Dataset/train/y_train.txt", col.names= "activity")
 
 #Read test and train and test data sets
-test_set<-read.table("./UCI HAR Dataset/test/X_test.txt")
-train_set<-read.table("./UCI HAR Dataset/train/X_train.txt")
+test_set<-read.table("./UCI HAR Dataset/test/X_test.txt", col.names=ft_names[,2])
+train_set<-read.table("./UCI HAR Dataset/train/X_train.txt", col.names=ft_names[,2])
 
 #Merge train and test sets and labels into one data frame
 merged_set<-rbind(train_set, test_set)
 merged_label<-rbind(train_label, test_label)
 merged_subjects<-rbind(subjects_train, subjects_test)
 merged_data<-cbind(merged_label, merged_subjects, merged_set)
-names(merged_data)<-c("label", "subjects", ft_names[,2])
 
 #Extract features measuring mean and standard deviation
-MxSD_data<-merged_data %>% select(label,contains("mean")|contains("std"))
+MxSD_data<-merged_data %>% select(activity, subjects ,contains("mean")|contains("std"))
 
 
