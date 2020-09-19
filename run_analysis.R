4cbbe8361f533586baf93ea00221b98a6204c1b0
BadFrameRate <rohatgi.aseem03@gmail.com>
2020-09-19 17:47:20 +0530

    read activity labels, features, test data and labels, and train data and labels

--- run_analysis.R
+++ run_analysis.R
@@ -5,3 +5,21 @@ if( !file.exists( "UCI HAR Dataset" )){
                  destfile="./data.zip" )
   unzip("./data.zip")
 }
+
+#Read file listing features
+ft_names<-read.table("./UCI HAR Dataset/features.txt")
+
+#Read file listing activity labels
+activity_labels<-read.table("./UCI HAR Dataset/activity_labels.txt", col.names=c("n","label"))
+
+#Read subject numbers for the data sets
+subjects_test<-read.table("./UCI HAR Dataset/test/subject_test.txt")
+subjects_train<-read.table("./UCI HAR Dataset/train/subject_train.txt")
+
+#Read test and train activity labels
+test_label<-read.table("./UCI HAR Dataset/test/y_test.txt")
+train_label<-read.table("./UCI HAR Dataset/train/y_train.txt")
+
+#Read test and train and test data sets
+test_set<-read.table("./UCI HAR Dataset/test/X_test.txt", col.names=ft_names[,2])
+train_set<-read.table("./UCI HAR Dataset/train/X_train.txt", col.names=ft_names[,2])
