a25b96744f7c7584347e6c3467d84997adb8c005
BadFrameRate <rohatgi.aseem03@gmail.com>
2020-09-19 18:04:08 +0530

    merged subject data for test and train together, and then added it to the previous dataframe; named columns for the new data frame

--- run_analysis.R
+++ run_analysis.R
@@ -21,13 +21,15 @@ test_label<-read.table("./UCI HAR Dataset/test/y_test.txt")
 train_label<-read.table("./UCI HAR Dataset/train/y_train.txt")
 
 #Read test and train and test data sets
-test_set<-read.table("./UCI HAR Dataset/test/X_test.txt", col.names=ft_names[,2])
-train_set<-read.table("./UCI HAR Dataset/train/X_train.txt", col.names=ft_names[,2])
+test_set<-read.table("./UCI HAR Dataset/test/X_test.txt")
+train_set<-read.table("./UCI HAR Dataset/train/X_train.txt")
 
 #Merge train and test sets and labels into one data frame
-merged_set<-rbind(train_set,test_set)
-merged_label<-rbind(train_label,test_label)
-merged_data<-cbind(merged_set,"label"=merged_label)
+merged_set<-rbind(train_set, test_set)
+merged_label<-rbind(train_label, test_label)
+merged_subjects<-rbind(subjects_train, subjects_test)
+merged_data<-cbind(merged_label, merged_subjects, merged_set)
+names(merged_data)<-c("label", "subjects", ft_names[,2])
 
 #Extract features measuring mean and standard deviation
 MxSD_data<-merged_data %>% select(label,contains("mean")|contains("std"))
